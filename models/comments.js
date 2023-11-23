8c2d5238aff7caec50d1331cc02323bcb702cf4c
Tatum Sterling <tatum.sterling29@gmail.com>
2023-11-22 22:06:01 -0600

    added comments model

--- models/comments.js
+++ models/comments.js
@@ -1,43 +1,49 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 
-class Posts extends Model {};
+class Comments extends Model {}
 
-Posts.init(
+Comments.init(
     {
         id: {
             type: DataTypes.INTEGER,
-            allowNull: false,
+            allownull: false,
             primaryKey: true,
             autoIncrement: true,
         },
-        title: {
-            type: DataTypes.STRING,
+        post_id: {
+            type: dataTypes.INTEGER,
+            referenes: {
+                model: 'post',
+                key: 'id',
+            },
+            allowNull: false,
+        },
+        user_id: {
+            type: dataType.INTEGER,
+            references: {
+                model: 'user',
+                key: 'id'
+            },
             allowNull: false,
         },
         content: {
             type: DataTypes.TEXT,
             allowNull: false,
         },
-        user_id: {
-            type: DataTypes.INTEGER,
-            references: {
-                model: 'user',
-                key: 'id'
-            },
-        },
         createdAt: {
             type: DataTypes.DATE,
             defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
         },
+
     },
     {
         sequelize,
         timestamps: false,
         freezeTableName: true,
         underscored: true,
-        modelName: 'post',
+        modelName: 'comment',
     }
 );
 
-module.exports = Posts;
+module.exports = Comments;
