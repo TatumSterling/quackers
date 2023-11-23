950233b77ac4c40597de5c73af8919e4e2441afe
Tatum Sterling <tatum.sterling29@gmail.com>
2023-11-22 23:39:30 -0600

    added seed data for categories

--- seeds/category-seeds.js
+++ seeds/category-seeds.js
@@ -1,1 +1,28 @@
-//create seed data for category model
+const { Category } = require('../models');
+
+const categoryData = [
+    { name: 'Programming Languages' },
+    { name: 'Web Development' },
+    { name: 'Mobile App Development' },
+    { name: 'Frameworks' },
+    { name: 'Database Management' },
+    { name: 'DevOps' },
+    { name: 'Software Architecture' },
+    { name: 'Cloud Computing' },
+    { name: 'Machine Learning and AI' },
+    { name: 'Version Control' },
+    { name: 'Cybersecurity' },
+    { name: 'Agile Development' },
+    { name: 'Code Reviews' },
+    { name: 'Coding Challenges' },
+    { name: 'Open Source Contribution' },
+    { name: 'Developer Tools' },
+    { name: 'Web Design' },
+    { name: 'Testing and QA' },
+    { name: 'Career Development' },
+    { name: 'Tech News and Trends' },
+  ];
+
+const seedCategories = () => Category.bulkCreate(categoryData);
+
+module.exports = seedCategories;
